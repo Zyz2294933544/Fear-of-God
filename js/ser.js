79050e0bafed8ca9f61690c0a3e330c05e668a3c
邢真杰 <1367261130@qq.com>
2021-12-10 19:43:19 +0800

    'submit'

---
+++ js/ser.js
@@ -0,0 +1,10 @@
+var ser1 = document.getElementsByClassName('ser')[0];
+var mb = document.getElementsByClassName('mb')[0];
+function serr() {
+    ser1.style.display = 'block';
+    mb.style.display = 'block';
+}
+function out() {
+    ser1.style.display = 'none';
+    mb.style.display = 'none';
+}
